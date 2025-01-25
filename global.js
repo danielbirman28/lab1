501147735df90e299ee2759bb31284397ff5019c
danielbirman28 <dbirman28@gmail.com>
2025-01-24 21:18:24 -0800

    fixed links?

--- global.js
+++ global.js
@@ -6,11 +6,11 @@ function $$(selector, context = document) {
 
 // Step 3.1: Define pages for the navigation menu
 let pages = [
-  { url: 'index.html', title: 'Home' },
-  { url: 'projects/index.html', title: 'Projects' },
-  { url: 'contact/index.html', title: 'Contact' },
-  { url: 'cv/index.html', title: 'Resume' },
-  { url: 'https://github.com/danielbirman28', title: 'GitHub' },
+  { url: 'portfolio/index.html', title: 'Home' },
+  { url: 'portfolio/projects/index.html', title: 'Projects' },
+  { url: 'portfolio/contact/index.html', title: 'Contact' },
+  { url: 'portfolio/cv/index.html', title: 'Resume' },
+  { url: 'portfolio/https://github.com/danielbirman28', title: 'GitHub' },
 ];
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
